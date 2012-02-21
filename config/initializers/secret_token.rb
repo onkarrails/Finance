eda521da10f88cf597a364fd7aed52f8378061a1
omkar <omkar.b440@gmail.com>
2012-02-21 18:30:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finance::Application.config.secret_token = 'e771e54f2c255e1397419ae245c3c3cf6ffafab3b8006b40b60cc4aae669308c3441242afe8c90500e3d0838b2ffa548e6e65f779ca445d2fae61a430437d139'
